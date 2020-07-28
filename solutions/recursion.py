fe5ca21ca07a2145f10200df2b23bb61cdcf8b96
Matthew Cooper <flux2341@gmail.com>
2020-07-28 14:23:45 -0700

    added solutions

---
+++ solutions/recursion.py
@@ -0,0 +1,67 @@
+
+
+
+
+
+
+
+# n! = n*(n-1)*(n-2)*...
+# 5! = 5*4*3*2*1
+# 10! = 10*9*8...
+# def factorial(n):
+#     output = 1
+#     while n > 0:
+#         output *= n
+#         n -= 1
+#     return output
+
+
+# def factorial(n, depth):
+#     print('\t'*depth, 'factorial(',n,')')
+#     if n == 1:
+#         print('\t'*depth, 'returning 1')
+#         return 1
+#     result = n*factorial(n-1, depth+1)
+#     print('\t'*depth, 'returning', result)
+#     return result
+
+# print(factorial(5, 1))
+
+# def say_hello():
+#     print('hello')
+#     main()
+
+# def main():
+#     x = 5
+#     say_hello()
+
+# main()
+
+
+
+
+# def c():
+#     message = 'c'
+#     print(message)
+#     print('returning from c')
+
+# def b():
+#     message = 'b'
+#     print(message)
+#     c()
+#     print('returning from b')
+
+# def a():
+#     message = 'a'
+#     print(message)
+#     b()
+#     print('returning from a')
+
+# a()
+# print('done')
+
+
+
+
+
+
