6ac082edb2dba10b76c2dfb658144ef739250038
Matthew Cooper <flux2341@gmail.com>
2020-08-03 09:25:32 -0700

    updated labs

--- labs/more_practice.md
+++ labs/more_practice.md
@@ -16,21 +16,20 @@ Convert a number of bytes `1567123` into a pretty form `1.5 mb`. The `round` fun
 A palindrome is a word that's the same forwards or backwards, e.g. racecar. Another way to think of it is as a `word that's equal to its own reverse`.
 
 
-Write a function `check_palindrome` which takes a string, and returns True if the string's a palindrome, or False if it's not.
+Write a function `check_palindrome(word)` which takes a string, and returns True if the string's a palindrome, or False if it's not.
 
 ```
->>> enter a word: racecar
->>> 'racecar' is a palindrome
-
->>> enter a word: palindrome
->>> 'palindrome' is not a palindrome
+enter a word: racecar
+'racecar' is a palindrome
+enter a word: palindrome
+'palindrome' is not a palindrome
 ```
 
 ## Problem 4: Anagram
 
 Two words are anagrams of eachother if the letters of one can be rearranged to fit the other. e.g. `anagram` and `nag a ram`.
 
-Write another function `check_anagram` that takes two strings as parameters and returns True if they're anagrams of eachother, False if they're not. The procedure for comparing the two strings is as follow:
+Write another function `check_anagram(word1, word2)` that takes two strings as parameters and returns True if they're anagrams of eachother, False if they're not. The procedure for comparing the two strings is as follow:
 
 1. Convert each word to lower case (`lower`)
 2. Remove all the spaces from each word (`replace`)
@@ -38,8 +37,8 @@ Write another function `check_anagram` that takes two strings as parameters and 
 4. Check if the two are equal
 
 ```
->>> enter the first word: anagram
->>> enter the second word: nag a ram
->>> 'anagram' and 'nag a ram' are anagrams
+enter the first word: anagram
+enter the second word: nag a ram
+'anagram' and 'nag a ram' are anagrams
 ```
 
--- solutions/practice2-strings.py
+++ solutions/practice2-strings.py
@@ -1,3 +1,5 @@
+
+# Problem 1 =========================================
 # Return the number of letter occurances in a string.
 
 def count_letter(letter, word):
@@ -11,6 +13,8 @@ def count_letter(letter, word):
 # print(count_letter('p', 'pneumonoultramicroscopicsilicovolcanoconiosis')) # 2
 
 
+
+# Problem 2 =======================================
 
 # print(ord('a')) # 97
 # print(ord('b')) # 98
@@ -48,6 +52,10 @@ def latest_letter(word):
 # print(latest_letter('pneumonoultramicroscopicsilicovolcanoconiosis')) # v
 
 
+
+
+# Problem 3 ======================================================
+
 # Write a function that returns the number of occurances of 'hi' in a given string.
 
 
@@ -74,9 +82,6 @@ def count_hi(text):
     # results = re.findall('hi', text)
     # return len(results)
 
-
-
-
     count = 0
     for i in range(len(text)-1):
         # print(len(text), i, i+1)
@@ -84,11 +89,6 @@ def count_hi(text):
         if text[i] == 'h' and text[i+1] == 'i':
             count += 1
     return count
-
-
-
-
-
 
 #               0123
 print(count_hi('hihih')) # 2
@@ -110,4 +110,106 @@ print(count_hi('hello23hi45world hi 123abcllamama hi')) # 3
 
 
 
+# Problem 5
+# Write a function that converts text to snake case (all lowercase, underscores for spaces, no special characters).
+​
+​
+# this_is_snake_case (python)
+# thisIsCamelCase (java, javascript, c#)
+# ThisIsPascalCase (python classes)
+# this-is-kebab-case (css)
+​
+​
+#text.replace(char.ascii)
+#string.ascii_punctuation()
+​
+import string
+​
+​
+def snake_case(text):
+    output_string = ''
+    for char in text:
+        if char not in string.punctuation:
+            output_string += char
+    output_string = output_string.lower()
+    output_string = output_string.replace(' ', '_')
+    #text = text.replace(string.punctuation, '')
+    return output_string
+#print(snake_case('@Hello % World!@#$!')) # hello_world
+#print(string.punctuation)
+​
+​
+# Problem 6
+​
+# Write a function that converts text to camel case (no spaces, no special characters, leading capitals except the first).
+​
+​
+​
+​
+def camel_case(text):
+    output_string = ''
+    text = text.lower() 
+    #remove punctuation
+    for char in text:
+        if char == ' ' or char not in string.punctuation:
+            output_string+= char
+    output_string_the_second = ''
+    list_output = output_string.split(' ')
+    for i in range(len(list_output)):
+        list_output[i] = list_output[i].capitalize()    
+    list_output[0] = list_output[0].lower()
+    output_string_the_second = ''.join(list_output)
+    #
+    # for i in range(len(output_string) - 1): #last letter?
+    #     if output_string[i] == ' ':
+    #         output_string_the_second += output_string[i+1].upper()
+    #         i += 1
+    #     else:
+    #         output_string_the_second += output_string[i]
+    return output_string_the_second   
+print(camel_case('Hello World!')) # helloWorld
+print(camel_case('This is another example.')) # thisIsAnotherExample
+​
+​
+​
+# mylist = ['apples', 'bananas', 'pears']
+# mylist[0] = 'cherries'
+# for i in range(len(mylist)):
+#     mylist[i] = mylist[i].capitalize()
+#     #mylist[i] = mylist[i] + '!'
+# print(mylist)
+​
+​
+# x = mylist[0]
+# x = 'hello!'
+# print(mylist[0])
+​
+# for element in mylist:
+#     element = 'hello!'
+# print(mylist)
+​
+# tester_string = "I love pizza!"
+# list_strings = tester_string.split(" ")
+# print(list_strings)
+# joined_strings = ''.join(list_strings)
+# print(joined_strings)
+​
+#"abcdefg"
+​
+#       01234
+# text = 'hello'
+# for char in text:
+#     print(char)
+# for i in range(len(text)):
+#     print(i, text[i])
+# 0123
+#'hihi'
+# def count_hi(text):
+#     count = 0
+#     for i in range(len(text)-1):
+#         # print(len(text), i, i+1)
+#         # text[i+1]
+#         if text[i] == 'h' and text[i+1] == 'i':
+#             count += 1
+#     return count
 
