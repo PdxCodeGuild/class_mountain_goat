52479f0ec822929d9e58f65e5f43542bdbf54da2
Matthew Cooper <flux2341@gmail.com>
2020-08-05 15:30:46 -0700

    update

--- solutions/demo_tuples.py
+++ solutions/demo_tuples.py
@@ -17,12 +17,22 @@ print(contact[2]) # green
 
 # contact.append(5) # crash
 
-for i in range(len(contact)):
-    print(contact[i])
+# for i in range(len(contact)):
+#     print(contact[i])
 
-for element in contact:
-    print(element)
+# for element in contact:
+#     print(element)
 
 
+
+
+def round_pad_zeroes(num, digits=2):
+    num = str(float(round(num, digits)))
+    num += '0'*(digits-len(num)+1+num.index('.'))
+    return num
+
+print(round_pad_zeroes(5, 3)) # 5.000
+print(round_pad_zeroes(3.1415963, 3)) # 3.142
+print(round_pad_zeroes(3.1, 5)) # 3.10000
 
 
