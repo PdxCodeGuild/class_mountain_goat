423655142a03ee635747711fa9d94c6a49226e5a
Matthew Cooper <flux2341@gmail.com>
2020-08-18 15:48:40 -0700

    added currency change

--- solutions/demo_requests.py
+++ solutions/demo_requests.py
@@ -25,21 +25,43 @@ import requests
 
 
 
-term = input('what is the term you\'d like to search for? ').lower()
-page = 1
-while True:
-    params = {'page': page, 'term': term}
-    headers = {'Accept': 'application/json'}
-    response = requests.get('https://icanhazdadjoke.com/search', headers=headers, params=params)
-    print(response.url)
+# term = input('what is the term you\'d like to search for? ').lower()
+# page = 1
+# while True:
+#     params = {'page': page, 'term': term}
+#     headers = {'Accept': 'application/json'}
+#     response = requests.get('https://icanhazdadjoke.com/search', headers=headers, params=params)
+#     print(response.url)
+#     data = response.json()
+#     for joke in data['results']:
+#         print(joke['joke'])
+#     total_pages = data["total_pages"]
+#     print(f'Page {page}/{total_pages}')
+#     next_page = input('would you like to see the next page? ').lower().strip()
+#     if next_page in ['y', 'yes', 'ok', 'sure']:
+#         page += 1
+#     else:
+#         break
+
+
+
+def get_country_from_currency(currency):
+    response = requests.get('https://restcountries.eu/rest/v2/currency/' + currency)
     data = response.json()
-    for joke in data['results']:
-        print(joke['joke'])
-    total_pages = data["total_pages"]
-    print(f'Page {page}/{total_pages}')
-    next_page = input('would you like to see the next page? ').lower().strip()
-    if next_page in ['y', 'yes', 'ok', 'sure']:
-        page += 1
-    else:
-        break
+    return [row['name'] for row in data]
+
+from_amount = float(input('what is the amount? '))
+from_currency = input('what is the currency?').upper().strip()
+print(get_country_from_currency(from_currency))
+to_currency = input('what is the currency you\'d like to convert to?').upper().strip()
+print(get_country_from_currency(to_currency))
+
+response = requests.get('https://api.exchangeratesapi.io/latest', params={'base': from_currency})
+rates = response.json()['rates']
+to_amount = rates[to_currency]*from_amount
+print(f'{from_amount} {from_currency} is {to_amount} {to_currency}')
+
+
+
+
 
